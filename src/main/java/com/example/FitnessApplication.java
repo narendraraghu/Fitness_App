71dce41e4f866e86f1ecafb79fe95f9b80631334
narendraraghu <narendraraghu.89@gmail.com>
2017-04-09 20:09:17 +0530

    first commit

---
+++ src/main/java/com/example/FitnessApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessApplication.class, args);
+	}
+}
